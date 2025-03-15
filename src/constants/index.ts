512a1e55c0178f8df7ce62b303689d890124e5da
Trigon Layer <160345451+TrigonLayer@users.noreply.github.com>
2025-03-15 03:28:20 +0000

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { ChainId, JSBI, Percent, Token, WETH } from '@nexuslayer/sdk'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x205544d0435aEdA54A6fcB86735a4d551013c587'
 
 // a list of tokens by chain
 type ChainTokenList = {
