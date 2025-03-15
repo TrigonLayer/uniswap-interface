4a5efda4cdf4e437c36f338b75003f6f94cdd64f
Trigon Araujo <misael18pereira@gmail.com>
2025-03-15 16:46:37 +0000

    first commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -142,4 +142,4 @@ export const MIN_ETH: JSBI = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(16))
 export const BETTER_TRADE_LINK_THRESHOLD = new Percent(JSBI.BigInt(75), JSBI.BigInt(10000))
 
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://unpkg.com/@uniswap/default-token-list@latest'
+export const DEFAULT_TOKEN_LIST_URL = 'https://cdn.jsdelivr.net/npm/@nexuslayer/token-list@latest'
